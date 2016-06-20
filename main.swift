869667ef3ec6974b7e32cc76576b7bf2312ac0fc
yan786362806 <786362806@qq.com>
2016-06-20 10:58:48 +0800

    两数相加
    忙了一上午，遇到各种问题，安装好7.3版本的xcode，更新了系统后，可以看视频学习了，简单的了解了swift语言，学习了几种基本用法，swif
    t和c++和java有很多相似的地方，学习起来不会太吃力，这个程序就是最基本的两个数相加，var是变量，let是常量

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  20141105044严伟1
+//
+//  Created by 20141105044y on 16/6/20.
+//  Copyright © 2016年 20141105044y. All rights reserved.
+//
+
+import Foundation
+
+var i = 2
+
+var p = 3
+
+let sum = i+p
+print("sum")
+
